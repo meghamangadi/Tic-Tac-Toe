4b8726749f91325284957505de2342a923a453e7
megha angadi <meghamangadi@gmail.com>
2021-08-30 09:16:40 +0530

    Added: Created empty Board for the Game

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,19 @@
 package com.blz.tictactoe;
 
 public class TicTacToe {
+	private static char[] element;
 
 	public static void main(String[] args) {
-	System.out.println("Welcome to the Tic Tac Toe");
-
+		System.out.println("Welcome to the Tic Tac Toe");
+		boardCreation();
 	}
 
+	private static void boardCreation() {
+		element = new char[10];
+		for (int i = 1; i < 10; i++) {
+			element[i] = ' ';
+			System.out.println(element[i]);
+		}
+
+	}
 }
