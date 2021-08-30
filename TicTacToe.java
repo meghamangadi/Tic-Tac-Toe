10e3f9559c1428ac4b952a299e4aff6d2d107c93
megha angadi <meghamangadi@gmail.com>
2021-08-30 10:21:00 +0530

    Added: Ability to allow the player to choose X or O

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,19 +1,42 @@
 package com.blz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
 	private static char[] board;
+	static char userMark, computerMark;
+	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe");
 		boardCreation();
+
+		choosingXorO();
 	}
 
 	private static void boardCreation() {
 		board = new char[10];
 		for (int i = 1; i < 10; i++) {
 			board[i] = ' ';
-			System.out.println(board[i]);
 		}
 
+	}
+
+	private static void choosingXorO() {
+		System.out.println("Choose 1 for 'X' or Choose 2 for 'O' as your mark");
+		int option = scan.nextInt();
+		switch (option) {
+		case 1:
+			userMark = 'X';
+			computerMark = 'O';
+			break;
+		case 2:
+			userMark = 'O';
+			computerMark = 'X';
+			break;
+		default:
+			System.out.println("Your input is invalid");
+			choosingXorO();
+		}
 	}
 }
