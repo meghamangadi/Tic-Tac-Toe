71784ca426f636b8fffd6f62932f2e789caf2fb8
megha angadi <meghamangadi@gmail.com>
2021-08-31 07:28:54 +0530

    Added: method tossCoin to check who plays the game first computer or user

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,12 +1,15 @@
 package com.blz.tictactoe;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
 	private static char[] board;
 	static char userMark, computerMark;
 	static Scanner scan = new Scanner(System.in);
-	static int userNumber;
+	private static final Random random = new Random();
+	static int userNumber, computerNumber;
+	static int turn = 1, flag = 0;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Tic Tac Toe Game");
@@ -16,6 +19,16 @@ public class TicTacToe {
 		currentBoard();
 		userCall();
 		userMove();
+		tossCoin();
+
+		while (flag == 0) {
+			if ((turn + 1) % 2 == 0) {
+				currentBoard();
+				userCall();
+				userMove();
+				turn++;
+			}
+		}
 	}
 
 	private static void boardCreation() {
@@ -81,5 +94,33 @@ public class TicTacToe {
 			board[userNumber] = userMark;
 			System.out.println(userMark + " user is marked " + userNumber);
 		}
+	}
+
+	private static void tossCoin() {
+		System.out.println("\nMaking a toss to check who plays first\nChoose 1 for Head or 2 for Tail");
+		int option = scan.nextInt();
+		if (option == 1 || option == 2) {
+			int flip = random.nextInt(2) + 1;
+			if (flip == 1) {
+				System.out.println("\nBy tossing Coin it shows HEAD\n");
+			} else {
+				System.out.println("\nBy tossing Coin it shows TAIL\n");
+			}
+			if (flip == option) {
+				System.out.println("User will start the game\n");
+			} else {
+				System.out.println("Computer will start the game\n");
+				computerFirstTurn();
+			}
+		} else {
+			System.out.println("\nInvalid input Again");
+			tossCoin();
+		}
+	}
+
+	public static void computerFirstTurn() {
+		computerNumber = random.nextInt(9) + 1;
+		board[computerNumber] = computerMark;
+		System.out.println("Computer choses '" + computerNumber + "' now user turn");
 	}
 }
